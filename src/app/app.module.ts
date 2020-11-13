ab3e366ed3b03ef91b88e1bd3cbef6958e65f826
gsosam99 <sosag@Guillermo-Sosa.local>
2020-11-13 18:30:21 -0400

    First Commit. MicroProyecto Inicializado con Firebase y Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// 1.- Import Libraries What You Need
+import { AngularFireModule, FirebaseApp } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// 2.- Import Environment Configuration
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // 3.- Inject Modules in Angular
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAAzX3Vbk_grPZn3IoQBR4ZCarWjTSpd6o",
+    authDomain: "microproyecto2-sosa.firebaseapp.com",
+    databaseURL: "https://microproyecto2-sosa.firebaseio.com",
+    projectId: "microproyecto2-sosa",
+    storageBucket: "microproyecto2-sosa.appspot.com",
+    messagingSenderId: "54988138734",
+    appId: "1:54988138734:web:04369d7549642c8eedabff",
+    measurementId: "G-SD0DKCBP7D"
+  }
 };
 
 /*
